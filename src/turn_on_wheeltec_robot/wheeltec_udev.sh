32032d3b358a4df3f595a8edfa5d01266cdb4d4a
ppppplus <63218106+ppppplus@users.noreply.github.com>
2021-09-08 21:57:58 +0800

    Add files via upload

---
+++ src/turn_on_wheeltec_robot/wheeltec_udev.sh
@@ -0,0 +1,7 @@
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0002", MODE:="0777", GROUP:="dialout", SYMLINK+="wheeltec_controller"' >/etc/udev/rules.d/wheeltec_controller.rules
+
+service udev reload
+sleep 2
+service udev restart
+
+
